d3f89d6771327846a4d820945320bbf18f7e0d07
Braulio Astudillo <brau97x@gmail.com>
2023-04-25 14:33:57 -0500

    juego2y3

--- src/app/usuario/menugame/ahorcadogame/ahorcadogame.page.ts
+++ src/app/usuario/menugame/ahorcadogame/ahorcadogame.page.ts
@@ -1,15 +1,451 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NavController } from '@ionic/angular';
+// Importación necesaria para mensajes flash.
+import { ToastController } from '@ionic/angular';
+// Importación necesaria para alerts.
+import { AlertController } from '@ionic/angular';
+import { Observable, timer } from 'rxjs';
+
 @Component({
   selector: 'app-ahorcadogame',
   templateUrl: './ahorcadogame.page.html',
   styleUrls: ['./ahorcadogame.page.scss'],
 })
-export class AhorcadogamePage implements OnInit {
+export class AhorcadogamePage  {
+  limite:number=1000;
+  min: number=0;
+  max: number=0;
+  score: number=0;
+  timer: any;
+  timerObs: any;
 
-  constructor() { }
+  // Definimos las variables
+  letra: string = '';
+  nombres: any = ['COCHE', 'CASA', 'PERRO', 'GATO', 'MURCIELAGO', 'IGUANA', 'SALTAMONTES', 'ESPEJO', 'LATIGO', 'ESPERANZA'];
+  nombreSecreto: any = this.palabraAleatoria(0, this.nombres.length - 1);
+  palabra: any = '';
+  muestraHuecos: any = this.muestraHuecosPalabra();
+  mensaje: string = '';
+  letras_utilizadas: string = '';
+  nombresecretomostrar: string = '';
 
-  ngOnInit() {
+  vidas: number = 6;
+  puntos: number = 0;
+  ganador: number = 0;
+  imagen: number = 1;
+
+  durationMessages: number = 3000;
+
+  // Creamos un array para guardar las letras que se van seleccionando.
+  controlLetras = new Array();
+
+  constructor(
+    public navCtrl: NavController,
+    private toastCtrl: ToastController,
+    public alertController: AlertController
+  ) {
+    this.startGame();
   }
+
+  //Metodo Tiempo H:m:s
+  texto:String="";
+  public convertirSegundos(segundos: number) {
+    const horas = Math.floor(segundos / 3600);
+    const minutos = Math.floor((segundos % 3600) / 60);
+    const segundosRestantes = segundos % 60;
+  this.texto=horas+"h:"+minutos+"m:"+segundosRestantes+"s"
+    return { horas, minutos, segundos: segundosRestantes };
+  }
+
+  //Metodo tiempo inicio juego
+  startGame() {
+    this.exitTimer();
+    this.score = 0;
+    this.timer = { current: 0, max: this.limite };
+    
+
+    const everySecond: Observable<number> = timer(0, 1000);
+    this.timerObs = everySecond.subscribe((seconds) => {
+      this.timer.current++;
+      this.convertirSegundos(this.timer.current)
+      if (this.timer.current === this.limite) {
+        this.exitTimer();
+        //this.gameFinished();
+      }
+    });
+
+  }
+
+//METODO Fin de tiempo 
+  exitTimer() {
+    this.timerObs?.unsubscribe();
+  }
+
+
+  // Método que valida la letra seleccionada.
+  public async compruebaLetra() {
+    // Formateamos a mayúsculas para mejorar la legibilidad.
+    let letraMayusculas = this.letra.toUpperCase();
+
+    // Si se ha seleccionado una letra...
+    if (letraMayusculas) {
+      if (this.controlLetras.indexOf(letraMayusculas) == -1) {
+        // Recorremos las letras de la palabra (array), para detectar si la letra se encuentra en ella.
+        if (this.nombreSecreto.indexOf(letraMayusculas) != -1) {
+          let nombreSecretoModificado = this.nombreSecreto;
+          let posicion = new Array();
+          let posicionTotal = 0;
+
+          let contador = 1;
+
+          while (nombreSecretoModificado.indexOf(letraMayusculas) != -1) {
+            posicion[contador] =
+              nombreSecretoModificado.indexOf(letraMayusculas);
+            nombreSecretoModificado = nombreSecretoModificado.substring(
+              nombreSecretoModificado.indexOf(letraMayusculas) +
+                letraMayusculas.length,
+              nombreSecretoModificado.length
+            );
+
+            // Calculamos la posición total.
+            if (contador > 1) {
+              posicionTotal = posicionTotal + posicion[contador] + 1;
+            } else {
+              posicionTotal = posicionTotal + posicion[contador];
+            }
+
+            // Preparamos la palabra para que sea mostrara en modal de solución directa.
+            this.palabra[posicionTotal] = letraMayusculas;
+
+            // Sumamos puntos
+            if (this.controlLetras.indexOf(letraMayusculas) == -1) {
+              this.puntos = this.puntos + 10;
+
+              // Hacemos uso de Toast Controller para lanzar mensajes flash.
+              let toast = this.toastCtrl.create({
+                message:
+                  'Genial, la letra ' +
+                  letraMayusculas +
+                  ' está en la palabra secreta.',
+                duration: this.durationMessages,
+                cssClass: 'toast-success',
+                position: 'top',
+              });
+              (await toast).present();
+            }
+
+            contador++;
+
+            // Si ya no quedan huecos, mostramos el mensaje para el ganador.
+            if (this.palabra.indexOf('_') == -1) {
+              // Sumamos puntos
+              if (this.controlLetras.indexOf(letraMayusculas) == -1) {
+                this.puntos = this.puntos + 50;
+              }
+
+              // Damos el juego por finalizado, el jugador gana.
+              this.finDelJuego('gana');
+            }
+          }
+        } else {
+          // Restamos una vida.
+          this.nuevoFallo();
+          // Actualizamos la imagen
+          this.nuevaImagen(this.imagen);
+
+          // Comprobamos si nos queda alguna vida.
+          if (this.vidas > 0) {
+            // Restamos puntos siempre y cuando no sean 0.
+            if (this.puntos > 0) {
+              if (this.controlLetras.indexOf(letraMayusculas) == -1) {
+                this.puntos = this.puntos - 2;
+              }
+            }
+
+            // Mostramos un mensaje indicando el fallo.
+            let toast = this.toastCtrl.create({
+              message:
+                'Fallo, la letra ' +
+                letraMayusculas +
+                ' no está en la palabra secreta. Recuerda que te quedan ' +
+                this.vidas +
+                ' vidas.',
+              duration: this.durationMessages,
+              cssClass: 'toast-danger',
+              position: 'top',
+            });
+            (await toast).present();
+          } else {
+            // Damos el juego por finalizado, el jugador pierde.
+            this.finDelJuego('pierde');
+          }
+        }
+
+        // Array de letras utilizadas para mostrar al jugador.
+        if (this.letras_utilizadas == '') {
+          this.letras_utilizadas += letraMayusculas;
+        } else {
+          this.letras_utilizadas += ' - ' + letraMayusculas;
+        }
+
+        // Añadimos al array de letras la nueva letra seleccionada.
+        this.controlLetras.push(letraMayusculas);
+      } else {
+        // En caso de que la letra ya hubiera sido seleccionada, mostramos un mensaje.
+        let toast = this.toastCtrl.create({
+          message:
+            'La letra ' +
+            letraMayusculas +
+            ' fue seleccionada anteriormente. Por favor, seleccione una letra diferente.',
+          duration: this.durationMessages,
+          cssClass: 'toast-warning',
+          position: 'top',
+        });
+        (await toast).present();
+      }
+    }
+  }
+
+  public muestraHuecosPalabra() {
+    let totalHuecos = this.nombreSecreto.length;
+
+    // Declaramos la variable huecos como nuevo array.
+    let huecos = new Array();
+    for (let i = 0; i < totalHuecos; i++) {
+      //Asignamos tantos huecos como letras tenga la palabra.
+      huecos.push('_');
+    }
+
+    // Para empezar formamos la variable palabra tan solo con los huecos, ya que en este momento aún no se ha seleccionado ninguna letra.
+    this.palabra = huecos;
+    return this.palabra;
+  }
+
+  // Método que genera una palabra aleatoria comprendida en el array nombres.
+  public palabraAleatoria(primer: number, ultimo: number) {
+    let numberOfName = Math.round(Math.random() * (ultimo - primer) + primer);
+    return this.nombres[numberOfName];
+  }
+
+  public nuevoFallo() {
+    this.vidas = this.vidas - 1;
+    return this.vidas;
+  }
+
+  public nuevaImagen(imagen: number) {
+    this.imagen = imagen + 1;
+    return this.imagen;
+  }
+
+  public confirmarResolver() {
+    
+
+
+    this.showPrompt()
+    
+    
+  }
+
+  public async showPrompt() {
+      const prompt = this.alertController.create({
+        //title: 'Solución directa',
+        message: '¿Está seguro de resolver la palabra secreta directamente?',
+        inputs: [
+          {
+            name: 'palabraSolucion',
+            id: 'palabraSolucion',
+            placeholder: this.palabra,
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancelar',
+            handler: (data) => {
+              // Se cierra ventana.
+            },
+          },
+          {
+            text: 'Resolver',
+            handler: (data) => {
+              // Llamamos a método que compara la palabra secreta con la insertada mediante teclado.
+              // var solucion = this.palabra.toString();
+              // var solucion = solucion.replace(/,/g, '');
+              var solucion = (
+                document.getElementById('palabraSolucion') as HTMLInputElement
+              ).value;
+              this.resolver(solucion);
+             
+            },
+            
+          },
+          
+        ],
+      });
+      (await prompt).present();    
+  }
+
+
+
+
+  public async showConfirm(accion: string) {
+    // Resolver
+    if (accion == 'resolver') {
+      const confirm = this.alertController.create({
+        //title: 'Solución directa',
+        message: '¿Está seguro de resolver la palabra secreta directamente?',
+        buttons: [
+          {
+            text: 'Cancelar',
+            handler: () => {
+              //
+            },
+          },
+          {
+            text: 'Confirmar',
+            handler: () => {
+              //
+            },
+          },
+        ],
+      });
+      (await confirm).present();
+    }
+  }
+
+  public async resolver(solucion: string) {
+    // Comprobamos la solución directa.
+
+    if (this.nombreSecreto == solucion.toUpperCase()) {
+      var totalOcultas = 0;
+      // Recorremos el array para detectar huecos sin transformar a letras.
+      for (var i = 0; i < this.palabra.length; i++) {
+        if (this.palabra[i] == '_') {
+          totalOcultas = totalOcultas + 1;
+        }
+      }
+
+      // ACIERTO :: Sumamos +50 y + 20 por cada hueco sin desvelar.
+      this.puntos = this.puntos + 50 + 20 * totalOcultas;
+
+      this.finDelJuego('gana');
+
+      // Colocamos la palabra secreta en el
+    } else {
+      // ERROR :: RESTAMOS 50.
+      this.puntos = this.puntos - 4;
+
+      if(this.puntos<0){
+        this.puntos=0
+        this.nuevaImagen(this.imagen);
+      }
+
+      this.vidas=this.vidas-1
+        this.exitTimer();
+        let toast = this.toastCtrl.create({
+          message:
+            'Lo sentimos!, La palabra ' +
+            solucion +
+            ' no es la palabra secreta. Su error le resta 4 puntos.',
+          duration: this.durationMessages,
+          cssClass: 'toast-danger',
+          position: 'top',
+        });
+        (await toast).present();
+      if(this.vidas==0)this.finDelJuego('pierde');
+    }
+    
+  }
+
+  public async finDelJuego(valor: string) {
+    
+    // Perdedor
+    if (valor == 'pierde') {
+      this.ganador = 0;
+      //final del tiempo
+      this.exitTimer();
+      
+      // Mostramos el mensaje como que el juego ha terminado
+      let toast = this.toastCtrl.create({
+        message:
+          'Perdiste!, Inténtalo de nuevo. Has conseguido un total de ' +
+          this.puntos +
+          ' puntos. La palabra secreta es ' +
+          this.nombreSecreto,
+        duration: this.durationMessages,
+        cssClass: 'toast-danger',
+        position: 'top',
+      });
+      (await toast).present();
+      this.gameFinished();
+    }
+
+    // Ganador
+    if (valor == 'gana') {
+      this.ganador = 1;
+      //final del tiempo
+      this.exitTimer();
+
+      let toast = this.toastCtrl.create({
+        message:
+          'Enhorabuena!, Has acertado la palabra secreta. Has conseguido un total de ' +
+          this.puntos +
+          ' puntos.',
+        duration: this.durationMessages,
+        cssClass: 'toast-success',
+        position: 'top',
+      });
+      (await toast).present();
+      this.gameFinished();
+    }
+    //this.gameFinished();
+    
+  }
+
+  public reiniciaJuego() {
+    this.letra = '';
+    this.palabra = '';
+    this.vidas = 6;
+    this.mensaje = '';
+    this.ganador = 0;
+    this.puntos = 0;
+    this.nombreSecreto = this.palabraAleatoria(0, this.nombres.length - 1);
+    this.muestraHuecos = this.muestraHuecosPalabra();
+    this.imagen = 1;
+    this.letras_utilizadas = '';
+    this.nombresecretomostrar = '';
+    this.controlLetras = new Array();
+    this.startGame();
+  }
+
+
+  async gameFinished() {
+    const alert = await this.alertController.create({
+      backdropDismiss: false,
+      header: 'Finished!',
+      message: '<ion-icon class="smiley" name="happy-outline"></ion-icon><br><div class="score">Score : ' + this.puntos+'</div>',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.navCtrl.navigateForward('/usuario-menu/usuario-menugames');
+          }
+        }, {
+          text: 'Play Again',
+          handler: () => {
+            this.startGame();
+            this.reiniciaJuego()
+            this.puntos=0
+            this.vidas=6
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+
+ 
 
 }
--- src/app/usuario/menugame/fruitsgame/fruitsgame.page.ts
+++ src/app/usuario/menugame/fruitsgame/fruitsgame.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-fruitsgame',
@@ -7,9 +10,236 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FruitsgamePage implements OnInit {
 
-  constructor() { }
+  limite:number=1000;
+  min: number=0;
+  max: number=0;
+  
+  timer: any;
+  timerObs: any;
+	
+	public score=0;
+	public gameState;       //estado del juego
+	public startGame1;       // para comenzar el juego mostrar display 
+	public countDown;       // cuenta regresiva de 3 seg
+	public totalTime;       // el tiempo total del jugador para ganar
+	public countTime;       // el tiempo del juego mientras se esta jugando
+	public shownTime;       // para mostrar el tiempo en texto
+	public interTime;       // temporizador  que comieza en 1 s
+	public interCount;      // temporizador en juego 
 
-  ngOnInit() {
-  }
+	public cardsTotal = 8;	// total de cartas dividas por 2  (asi habra 2 combinaciones total 12)
+	public cardsArray = [];	// almacenamiento de todos los pares de tarjetas
+	public userLife = 6;		// cantidad de intentos del usuario
+	public imageDir = '../assets/img/FruitsAll/'; //direccion de todas las imagenes 
+	public images = ['banano', 'cereza', 'durazno', 'fresa',
+		'kiwi', 'limon', 'mango', 'manzana'];
+
+	public selectCard1pos = -1;	// selecionamos tarjeta #1 position
+	public selectCard1val = -1;	// selecionamos tarjeta #1 value
+	public selectCard2pos = -1;	// selecionamos tarjeta #2 position
+	public selectCard2val = -1;	// selecionamos tarjeta #2 value
+	public selectOldPosix = -1; 	// seleccionamos la vieja posicion
+	public debugText = "Debug text goes here! :)"
+
+	constructor(
+		public alertController: AlertController,  public navCtrl: NavController,
+	) {
+		this.startGame();
+	 }
+
+	ngOnInit() {
+		this.restartGame();
+	}
+
+
+
+	//funcion para completar la matriz de tarjetas con pares
+	// de posicion y valor de 0 a 6  lo que significa es que para cada
+	//dos posiciones vamos a querer el mismo valor 
+
+	populateCards() {
+		this.cardsArray = [];
+		var x = 0;
+		var y = 0;
+		for (var i = 0; i < this.cardsTotal; i++) {
+			// poner  la tarjeta a la matriz y asignar valor
+			this.cardsArray.push({ pos: i, val: y });
+			// Voltear x para asignar el mismo valor a la siguiente carta
+			if (x == 0) x = 1;
+			else { x = 0; y++ }
+			console.log("**** this cards array")
+			console.log(this.cardsArray)
+		}
+	}
+
+	// Function para seleccionar las tarjetas
+	selectCard(pos, val, i) {
+		var actOne = false;
+		//codigo para seleccionar la segunda tarjeta 
+		if (this.selectCard1pos > -1 && this.selectCard2pos == -1) {
+			this.selectCard2pos = pos;
+			this.selectCard2val = val;
+			actOne = true;
+		}
+		// Code to select the first card
+		if (this.selectCard1pos == -1 && !actOne) {
+			this.selectCard1pos = pos;
+			this.selectCard1val = val;
+			this.selectOldPosix = i;
+		}
+		//si tenemos las dos cartas seleccionadas
+		// verificamos si coinciden o fallan 
+
+		if (actOne && this.selectCard1pos > -1 && this.selectCard2pos > -1) {
+			setTimeout(() => {
+				// si las dos cartas coninciden , ahcer esto
+				if (this.selectCard1val == this.selectCard2val) {
+					this.debugText = "!Las cartas coinciden!";
+					//elimnar tarjetas del arraya de tarjetas
+					this.score +=7;
+					this.cardsArray.splice(this.selectOldPosix, 1, { pos: this.selectOldPosix, val: -1 });
+					this.cardsArray.splice(i, 1, { pos: i, val: -1 });
+					this.resetSelects();
+					this.winCon()
+				}
+				// de lo contrario tome una vida y reinicie
+				else {
+					this.debugText = "!las tarjetas no coinciden!";
+					this.userLife -= 1;
+					this.score-=2
+					if (this.score <= 0) this.score=0;
+					this.resetSelects();
+					if (this.userLife <= 0) this.loseCon();
+				}
+			}, 1000);
+		}
+	}
+
+
+	//funcion para barajar o ramdorizar el array de cartas 
+	shuffle(a) {
+		var j, x, i;
+		for (i = a.length; i; i--) {
+			j = Math.floor(Math.random() * i);
+			x = a[i - 1];
+			a[i - 1] = a[j];
+			a[j] = x;
+		}
+	}
+
+	// funcion para reiniciar el juego 
+	restartGame() {
+		this.gameState = 'load';
+		this.startGame1 = false;
+		
+
+		this.userLife = 6;
+		this.resetSelects();
+		this.populateCards();
+		this.shuffle(this.cardsArray);
+		this.shuffle(this.images);
+
+		this.startGame()
+		
+		
+
+		//la cuencta del tiempo una ves el juego inicie 
+		
+	}
+
+	// Condicion para ganar
+	winCon() {
+		var winCheck = false;
+		//si al menos una o mas cartas no se an resuelto estan doferentes de -1 
+		//entonces el usuario aun no a ganado
+		for (var i = 0; i < this.cardsArray.length; i++)
+			if (this.cardsArray[i].val != -1){ 
+			winCheck = true;
+			}
+		
+		// si el control es falso , el jugador a ganado el juego
+		if (winCheck == false) {
+		this.gameState = 'win';
+		this.gameFinished()
+		}
+		
+		this.exitTimer()
+		
+	}
+
+	// condicion de perdida
+	loseCon() {
+		this.gameState = 'lose';
+		if(this.userLife==0)this.gameFinished()
+		this.exitTimer()
+	}
+
+	// Function funcion para resetear las caras deleccionadas
+	resetSelects() {
+		this.selectCard1pos = -1;	// Selected card #1 position
+		this.selectCard1val = -1;	// Selected card #1 value
+		this.selectCard2pos = -1;	// Selected card #2 position
+		this.selectCard2val = -1;	// Selected card #2 value
+	}
+
+	texto:String="";
+	public convertirSegundos(segundos: number) {
+		const horas = Math.floor(segundos / 3600);
+		const minutos = Math.floor((segundos % 3600) / 60);
+		const segundosRestantes = segundos % 60;
+	this.texto=horas+":"+minutos+":"+segundosRestantes
+		return { horas, minutos, segundos: segundosRestantes };
+	}
+
+	startGame() {
+		this.exitTimer();
+		this.score = 0;
+		this.timer = { current: 0, max: this.limite };
+		
+	
+		const everySecond: Observable<number> = timer(0, 1000);
+		this.timerObs = everySecond.subscribe((seconds) => {
+		  this.timer.current++;
+		  this.convertirSegundos(this.timer.current)
+		  if (this.timer.current === this.limite) {
+			this.exitTimer();
+			//this.gameFinished();
+		  }
+		});
+	
+	  }
+
+
+	exitTimer() {
+		this.timerObs?.unsubscribe();
+	  }
+	
+
+
+	  async gameFinished() {
+		const alert = await this.alertController.create({
+		  backdropDismiss: false,
+		  header: 'Finished!',
+		  message: '<ion-icon class="smiley" name="happy-outline"></ion-icon><br><div class="score">Score : ' + this.score+'</div>',
+		  buttons: [
+			{
+			  text: 'Cancel',
+			  role: 'cancel',
+        handler: () => {
+          this.navCtrl.navigateForward('/usuario-menu/usuario-menugames');
+        }
+			}, {
+			  text: 'Play Again',
+			  handler: () => {
+				this.startGame();
+				this.restartGame()
+				this.score=0
+				this.userLife=6
+			  }
+			}
+		  ]
+		});
+		await alert.present();
+	  }
 
 }
